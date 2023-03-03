9695d471210fbbe06b9160207922cb343f9bb432
Dani <96537843+DaniGP17@users.noreply.github.com>
2023-03-03 23:15:26 +0100

    Convert hash to readable path

---
+++ REAssetRipper.Core/Handlers/List.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace REAssetRipper.Core.Handlers
+{
+	public static class List
+	{
+        public static Dictionary<string, string> hashList = new Dictionary<string, string>();
+		public static void ReadList()
+		{
+            string filePath = "/Users/danielgallegopinilla/Documents/GitHub/REAssetRipper/REAssetRipper.Core/HashList/re7.list";
+            StreamReader reader = new StreamReader(filePath);
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                int spaceIndex = line.IndexOf(' ');
+                string hash = line.Substring(0, spaceIndex);
+                string path = line.Substring(spaceIndex + 1);
+                hashList.Add(hash, path);
+            }
+
+            reader.Close();
+        }
+
+        public static string GetNameFromHash(string hash)
+        {
+            if (hashList.Count == 0)
+            {
+                ReadList();
+            }
+            string path;
+            if (hashList.TryGetValue(hash, out path))
+            {
+                return path;
+            }
+            return hash;
+        }
+	}
+}
+
--- REAssetRipper.Testing/Program.cs
+++ REAssetRipper.Testing/Program.cs
@@ -9,8 +9,8 @@ using System.Text;
 
 Pak pak = new Pak("/Users/danielgallegopinilla/Desktop/re_chunk_000.pak");
 List<Asset> results = new List<Asset>(pak.PakAssets.Count);
-
+List.ReadList();
 foreach (var entry in pak.PakAssets)
 {
-    Console.Write(entry.Value.LowerCaseHash + "  " + entry.Value.UpperCaseHash + "\n");
+    Console.Write(List.GetNameFromHash(entry.Value.LowerCaseHash.ToString()) + "\n");
 }
