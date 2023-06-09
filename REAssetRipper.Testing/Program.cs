75a6ddee0da06f37e05d217756f7b7a2a169f3e1
Daniel GP <dani.dcshop.gg@gmail.com>
2023-06-09 20:27:26 +0200

    Upload last changes scene support

--- REAssetRipper.Testing/Program.cs
+++ REAssetRipper.Testing/Program.cs
@@ -1,47 +1,213 @@
-﻿using REAssetRipper.Core.Handlers;
-using REAssetRipper.Core.Constants;
-using System;
-using System.Text.RegularExpressions;
-using REAssetRipper.Core.Logs;
-using System.IO;
-using PhilLibX.IO;
-using PhilLibX.Imaging;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.SqlServer.Server;
-using System.Drawing;
-using static REAssetRipper.Core.Constants.Structures;
-using System.Diagnostics;
-using System.Xml.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
+using PhilLibX.Mathematics;
+using PhilLibX.IO;
+using PhilLibX;
+using System.Text;
 
 namespace REAssetRipper.Testing
 {
     public class Program
     {
-        private static REAssetRipper.Core.Handlers.Pak pak;
-        static void Main(string[] args)
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct MeshHeaderRE7
         {
-            pak = new REAssetRipper.Core.Handlers.Pak("\\\\Mac\\Home\\Desktop\\re_chunk_000.pak");
-            List<Asset> results = new List<Asset>(pak.PakAssets.Count);
-            REAssetRipper.Core.Handlers.List.ReadList();
-            int asd = 0;
-            int lastid = 0;
+            public uint Magic;
+            public uint Version;
+            public int FileSize;
+            public ushort Unk;
+            public ushort StringCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public long[] ModelPointers;
+            public long BoneDataHeaderPointer;
+            public long UnkPointer01;
+            public long UnkPointer02;
+            public long GeometryPointer;
+            public long UnkPointer03;
+            public long MaterialNamesPointer;
+            public long BoneNamesPointer;
+            public long UnkPointer04;
+            public long StringTablePointer;
+        }
 
-            foreach (var entry in pak.PakAssets)
-            {
-                string path = List.GetNameFromAsset(entry.Value);
-                if (AssetClasificator.GetType(path) == AssetTypes.types.Texture && path.Contains(".mdf."))
-                {
-                    ExtractAsset(entry.Value, "\\\\Mac\\Home\\Desktop\\test.mdf.8");
-                    //File.WriteAllBytes("\\\\Mac\\Home\\Desktop\\sm2080_cardkey01a.tex.8", pak.LoadAsset(entry.Value));
-                    Debug.WriteLine(entry.Value.DecompressedSize + "  " + entry.Value.CompressedSize);
-                    break;
-                }
-            }
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct BoneDataHeaderRE7
+        {
+            public short BoneCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 14)]
+            public byte[] Padding;
+            public long BoneTablePointer;
+            public long MatricesPointer;
+            public long UnkMatricesPointer01;
+            public long UnkMatricesPointer02;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 16)]
+        internal struct BoneDataRE7
+        {
+            public short BoneIndex;
+            public short ParentIndex;
+            public short NextSibling;
+            public short FirstChild;
+            public short Unk; // Sometimes == BoneIndex, Sometimes -1
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct ModelHeaderRE7
+        {
+            public byte LODCount;
+            public byte MaterialCount;
+            public byte UVCount;
+            public byte Unk01;
+            public int Unk02;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct LODHeaderRE7
+        {
+            public ushort MeshCount;
+            public byte Flags;
+            public byte BoneCount;
+            public float LODDistance;
+            public long MeshesPointer;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct LODMeshRE7
+        {
+            public byte MeshIndex;
+            public byte SubMeshCount;
+            public byte Unk01;
+            public byte Unk02;
+            public int Unk03;
+            public int VertexCount;
+            public int FaceCount;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct LODSubMeshRE7
+        {
+            public int MaterialIndex;
+            public int FaceCount;
+            public int FaceIndex;
+            public int VertexIndex;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct GeometryHeaderRE7
+        {
+            public uint VertexBufferSize;
+            public long UnkPointer;
+            public int Padding;
+            public int FaceBufferSize;
+            public long UnkPointer01;
+            public int Padding01;
+            public long Unk01;
+            public long FaceBufferOffset;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct Matrix4x4
+        {
+            public Vector4 X;
+            public Vector4 Y;
+            public Vector4 Z;
+            public Vector4 W;
+
+           
         }
 
 
-        private static void ExtractAsset(PakAssets asset, string path)
+        private static REAssetRipper.Core.Handlers.Pak pak;
+        public static void MurmurHash3_x86_32(byte[] key, int len, uint seed, out uint result)
+        {
+            const uint c1 = 0xcc9e2d51;
+            const uint c2 = 0x1b873593;
+
+            uint h1 = seed;
+            int nblocks = len / 4;
+
+            // body
+            for (int i = 0; i < nblocks; i++)
+            {
+                uint k1 = BitConverter.ToUInt32(key, i * 4);
+
+                k1 *= c1;
+                k1 = rotl32(k1, 15);
+                k1 *= c2;
+
+                h1 ^= k1;
+                h1 = rotl32(h1, 13);
+                h1 = h1 * 5 + 0xe6546b64;
+            }
+
+            // tail
+            uint k2 = 0;
+            int tailIndex = nblocks * 4;
+            switch (len & 3)
+            {
+                case 3:
+                    k2 ^= (uint)key[tailIndex + 2] << 16;
+                    goto case 2;
+                case 2:
+                    k2 ^= (uint)key[tailIndex + 1] << 8;
+                    goto case 1;
+                case 1:
+                    k2 ^= key[tailIndex];
+                    k2 *= c1;
+                    k2 = rotl32(k2, 15);
+                    k2 *= c2;
+                    h1 ^= k2;
+                    break;
+            }
+
+            // finalization
+            h1 ^= (uint)len;
+
+            // fmix32
+            h1 ^= h1 >> 16;
+            h1 *= 0x85ebca6b;
+            h1 ^= h1 >> 13;
+            h1 *= 0xc2b2ae35;
+            h1 ^= h1 >> 16;
+
+            result = h1;
+        }
+
+        private static uint rotl32(uint x, byte r)
+        {
+            return (x << r) | (x >> (32 - r));
+        }
+
+        public static uint ConvertFilePathToMurmurHash(string filePath, bool lower, bool changeCase, bool keepNullTerminator)
+        {
+            // We'll need a UTF-16 variant of the filepath for this
+            filePath = filePath.Replace('\\', '/');
+            if (changeCase)
+            {
+                if (lower) filePath = filePath.ToLower();
+                else filePath = filePath.ToUpper();
+            }
+
+            byte[] wideString = Encoding.Unicode.GetBytes(filePath);
+            uint murmurHash = 0;
+            if (keepNullTerminator == false)
+                MurmurHash3_x86_32(wideString, wideString.Length - 2, 0xFFFFFFFF, out murmurHash);
+            else
+                MurmurHash3_x86_32(wideString, wideString.Length, 0xFFFFFFFF, out murmurHash);
+
+            return murmurHash;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertFilePathToMurmurHash("natives/x64/aaa/ambassadortrial_tu/animation/player/pl0000/motfsm/pl0000_interact_lookmole_v2.motfsm.17", true, true, false));
+        }
+
+
+        /*private static void ExtractAsset(PakAssets asset, string path)
         {
             AssetTypes.types type = AssetClasificator.GetType(path);
             switch (type)
@@ -54,6 +220,6 @@ namespace REAssetRipper.Testing
                     image.Save(path + "." + REAssetRipper.Core.Helpers.Settings.textureExport.ToString());
                     break;
             }
-        }
+        }*/
     }
 }
--- REAssetRipper.Testing2/Program.cs
+++ REAssetRipper.Testing2/Program.cs
@@ -1,74 +1,240 @@
 ﻿using REAssetRipper.Core.Handlers;
 using REAssetRipper.Core.Constants;
-using System;
-using System.Text.RegularExpressions;
-using REAssetRipper.Core.Logs;
-using System.IO;
-using PhilLibX.IO;
 using PhilLibX.Imaging;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.SqlServer.Server;
-using System.Drawing;
+using System.IO;
 using static REAssetRipper.Core.Constants.Structures;
-using System.Diagnostics;
-using System.Xml.Linq;
-using System.Runtime.Remoting.Messaging;
+using System;
+using System.CodeDom;
 using System.Text;
+using PhilLibX;
+using System.Runtime.Remoting.Messaging;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace REAssetRipper.Testing
 {
+    public class GameObjectInfoStruct
+    {
+        public byte[] bytesGuid;
+        public int objectID;
+        public int parentID;
+        public ushort componentCount;
+        public short ukn;
+        public int prefabID;
+        public GameObjectInfoStruct(byte[] bytesGuid, int objectID, int parentID, ushort componentCount, short ukn, int prefabID)
+        {
+            this.bytesGuid = bytesGuid;
+            this.objectID = objectID;
+            this.parentID = parentID;
+            this.componentCount = componentCount;
+            this.ukn = ukn;
+            this.prefabID = prefabID;
+        }
+    }
+
+    public class FolderInfoStruct
+    {
+        public int objectID;
+        public int parentID;
+        public FolderInfoStruct(int objectID, int parentID)
+        {
+            this.objectID = objectID;
+            this.parentID = parentID;
+        }
+    }
+
+    public class ObjectParser
+    {
+        public string Key { get; set; }
+        public string Crc { get; set; }
+        public List<object> Fields { get; set; }
+        public string Name { get; set; }
+    }
+
     public class Program
     {
+        private static GameObjectInfoStruct[] gameObjectInfo;
+        private static FolderInfoStruct[] folderInfo;
+        private static Tuple<string, int>[] resourceInfos;
+        private static Tuple<string, UInt32>[] prefabInfos;
+        private static UInt32[] objectTable;
+
         private static REAssetRipper.Core.Handlers.Pak pak;
         static void Main(string[] args)
         {
-            pak = new REAssetRipper.Core.Handlers.Pak("D:\\Games\\Resident Evil VII Biohazard\\re_chunk_000.pak");
-            List<Asset> results = new List<Asset>(pak.PakAssets.Count);
-            REAssetRipper.Core.Handlers.List.ReadList();
+            FileStream fileStream = new FileStream("E:\\Program Files (x86)\\Steam\\steamapps\\common\\RESIDENT EVIL 7 biohazard\\Retool\\RESteam\\re_chunk_000\\natives\\stm\\environment\\scene\\chapter4\\c04_cottageswamp01.scn.20", FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            uint magic = binaryReader.ReadUInt32();
+            int infoCount = binaryReader.ReadInt32();
+            int resourceCount = binaryReader.ReadInt32();
+            int folderCount = binaryReader.ReadInt32();
+            int prefabCount = binaryReader.ReadInt32();
+            int userdataCount = binaryReader.ReadInt32();
+            UInt64 folderInfoTbl = binaryReader.ReadUInt64();
+            UInt64 resourceInfoTbl = binaryReader.ReadUInt64();
+            UInt64 prefabInfoTbl = binaryReader.ReadUInt64();
+            UInt64 userdataInfoTbl = binaryReader.ReadUInt64();
+            UInt64 dataOffset = binaryReader.ReadUInt64();
+            Console.WriteLine("magic: {0}", magic);
+            Console.WriteLine("infoCount: {0}", infoCount);
+            Console.WriteLine("resourceCount: {0}", resourceCount);
+            Console.WriteLine("folderCount: {0}", folderCount);
+    
+            Console.WriteLine("prefabCount: {0}", prefabCount);
+            Console.WriteLine("userdataCount: {0}", userdataCount);
+            Console.WriteLine("folderInfoTbl: {0}", folderInfoTbl);
+            Console.WriteLine("resoureceInfoCount: {0}", resourceInfoTbl);
+            Console.WriteLine("prefabInfoTbl: {0}", prefabInfoTbl);
+            Console.WriteLine("userdataInfoTbl: {0}", userdataInfoTbl);
+            Console.WriteLine("dataOffset: {0}", dataOffset);
+            Console.WriteLine("__________________________________________");
 
-            foreach (var entry in pak.PakAssets)
+            //Read GameObjectInfos
+            Console.WriteLine("Reading GameObject Infos");
+            gameObjectInfo = new GameObjectInfoStruct[infoCount];
+            for (int i = 0; i < infoCount; i++)
             {
-                string path = List.GetNameFromAsset(entry.Value);
-                if (AssetClasificator.GetType(path) == AssetTypes.types.AudioBnk)
-                {
-                    Console.WriteLine(path);
-                    byte[] bnkBytes = pak.LoadAsset(entry.Value);
-                    BNKHeaderRE7 bnkHeader;
-                    MemoryStream memoryStream = new MemoryStream(bnkBytes);
-                    BinaryReader binaryReader = new BinaryReader(memoryStream);
-                    bnkHeader = binaryReader.ReadStruct<BNKHeaderRE7>();
-                    byte[] magicBytes = BitConverter.GetBytes(bnkHeader.Magic);
-                    int magicInt = BitConverter.ToInt32(magicBytes, 0);
-                    string magicStr = new string(new[] { (char)(magicInt & 0xff), (char)((magicInt >> 8) & 0xff), (char)((magicInt >> 16) & 0xff), (char)((magicInt >> 24) & 0xff) });
-                    if (magicStr != "BKHD")
-                        return;
-
-                    binaryReader.BaseStream.Seek(bnkHeader.TableOffset, SeekOrigin.Begin);
-                    BNKIndexEntryRE7[] sounds = new BNKIndexEntryRE7[bnkHeader.TableCount];
-                    for (int i = 0; i < bnkHeader.TableCount; i++)
-                    {
-                        sounds[i].Offset = binaryReader.ReadInt32();
-                        sounds[i].Size = binaryReader.ReadInt32();
-                        sounds[i].Flags = binaryReader.ReadInt32();
-
-                        int nameLength = binaryReader.ReadInt32();
-                        sounds[i].Name = Encoding.ASCII.GetString(binaryReader.ReadBytes(nameLength));
-                    }
-
-                    // Imprimir nombres de archivo de sonido
-                    Console.WriteLine("Archivos de sonido encontrados en el archivo .bnk:");
-                    foreach (BNKIndexEntryRE7 sound in sounds)
-                    {
-                        Console.WriteLine(sound.Name);
-                    }
-
-                    // Cerrar archivo y liberar recursos
-                    binaryReader.Close();
-                    break;
-                }
+                byte[] bytesGuid = binaryReader.ReadBytes(16);
+                int objectID = binaryReader.ReadInt32();
+                int parentId = binaryReader.ReadInt32();
+                ushort componentCount = binaryReader.ReadUInt16();
+                short ukn = binaryReader.ReadInt16();
+                int prefabID = binaryReader.ReadInt32();
+                gameObjectInfo[i] = new GameObjectInfoStruct(bytesGuid, objectID, parentId, componentCount, ukn, prefabID);
             }
 
+            Console.WriteLine("Reading Folder Infos");
+            //Read FolderInfos
+            folderInfo = new FolderInfoStruct[folderCount];
+            for (int i = 0; i < folderCount; i++)
+            {
+                int objectID = binaryReader.ReadInt32();
+                int parentID = binaryReader.ReadInt32();
+
+                folderInfo.Append(new FolderInfoStruct(objectID, parentID));
+            }
+
+            //Skip 8 empty bytes
+            binaryReader.ReadUInt64();
+
+            Console.WriteLine("Reading Resources Infos");
+            resourceInfos = new Tuple<string, int>[resourceCount];
+            long currentSeekPosition = binaryReader.BaseStream.Position;
+            for (int i = 0; i < resourceCount; i++)
+            {
+                currentSeekPosition = binaryReader.BaseStream.Position;
+                UInt64 strOffset = binaryReader.ReadUInt64();
+                binaryReader.BaseStream.Seek((int)strOffset, SeekOrigin.Begin);
+                string pathStr = "";
+                while (true)
+                {
+                    int charCodified = binaryReader.ReadInt16();
+                    if(charCodified == 0)
+                    {
+                        break;
+                    }
+                    char charConverted = Encoding.ASCII.GetChars(new byte[] { (byte)charCodified })[0];
+                    pathStr += charConverted;
+                }
+                resourceInfos[i] = Tuple.Create(pathStr, (int)strOffset);
+                binaryReader.BaseStream.Seek(currentSeekPosition + 8, SeekOrigin.Begin);
+            }
+
+            //Skip 8 bytes because are empty
+            binaryReader.ReadUInt64();
+
+            Console.WriteLine("Reading Prefab Infos");
+            prefabInfos = new Tuple<string, UInt32>[prefabCount];
+            //Read FolderInfos
+            for (int i = 0; i < prefabCount; i++)
+            {
+                UInt32 strOffset = binaryReader.ReadUInt32();
+                int parentID = binaryReader.ReadInt32();
+
+                binaryReader.BaseStream.Seek((int)strOffset, SeekOrigin.Begin);
+                string pathStr = "";
+                while (true)
+                {
+                    int charCodified = binaryReader.ReadInt16();
+                    if (charCodified == 0)
+                    {
+                        break;
+                    }
+                    char charConverted = Encoding.ASCII.GetChars(new byte[] { (byte)charCodified })[0];
+                    pathStr += charConverted;
+                }
+                prefabInfos[i] = Tuple.Create(pathStr, strOffset);
+                binaryReader.BaseStream.Seek(currentSeekPosition + 24, SeekOrigin.Begin);
+            }
+
+
+            //Move binary reader position to data
+            binaryReader.BaseStream.Seek((long)dataOffset, SeekOrigin.Begin);
+
+            Console.WriteLine("Reading Data Header");
+            //Read Data Header
+            uint magicData = binaryReader.ReadUInt32();
+            uint versionData = binaryReader.ReadUInt32();
+            int objectCountData = binaryReader.ReadInt32();
+            int instanceCountData = binaryReader.ReadInt32();
+            binaryReader.ReadUInt32();
+            int userdataCountData = binaryReader.ReadInt32();
+            uint instanceOffsetData= binaryReader.ReadUInt32() + 18160;
+            uint dataOffsetData = binaryReader.ReadUInt32() + 18160;
+            uint userdataOffsetData = binaryReader.ReadUInt32() + 18160;
+
+
+            //Skip 12 ukn bytes
+            binaryReader.ReadBytes(12);
+
+
+            Console.WriteLine("Reading Object Table");
+            objectTable = new UInt32[objectCountData];
+            //Read FolderInfos
+            for (int i = 0; i < objectCountData; i++)
+            {
+                objectTable[i] = binaryReader.ReadUInt32();
+            }
+
+            //Move binary reader position to instance info data
+            binaryReader.BaseStream.Seek((long)instanceOffsetData, SeekOrigin.Begin);
+
+            string parserData = System.IO.File.ReadAllText("DataParser/rszre7rt.json");
+            Dictionary<string, ObjectParser> objectParsers = JsonConvert.DeserializeObject<Dictionary<string, ObjectParser>>(parserData);
+            /*foreach(KeyValuePair<string, ObjectParser> pair in objectParsers)
+            {
+                Console.WriteLine("Key: {0}", pair.Key);
+                Console.WriteLine("CRC: {0}", pair.Value.Crc);
+                Console.WriteLine("Name: {0}", pair.Value.Name);
+                Console.WriteLine("Fields:");
+
+                foreach (object field in pair.Value.Fields)
+                {
+                    Console.WriteLine("- {0}", field);
+                }
+
+                Console.WriteLine();
+            }*/
+
+
+            Console.WriteLine("Reading Instance Infos");
+            for (int i = 0; i < instanceCountData; i++)
+            {
+                string instanceHash = binaryReader.ReadUInt32().ToString("X").PadLeft(8, '0').ToLower();
+                uint instanceCRC = binaryReader.ReadUInt32();
+                /*Console.WriteLine("instanceHash: " + instanceHash);
+                Console.WriteLine("instanceCRC: " + instanceCRC);*/
+                //Console.WriteLine(instanceHash.ToString("X").PadLeft(8, '0').ToLower());
+                if (objectParsers.ContainsKey(instanceHash))
+                {
+                    //Console.WriteLine(objectParsers[instanceHash].Name);
+                }
+                else
+                {
+                    Console.WriteLine("Can't find game object of type: " + instanceHash);
+                }
+            }
         }
 
 
